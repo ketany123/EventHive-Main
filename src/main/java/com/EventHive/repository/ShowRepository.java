317e5d974ea6fad5d6045b7c26dbe67f27159d4c
ketany123 <ketanyelmate4@gmail.com>
2025-03-21 22:32:37 +0530

    Repositories added

---
+++ src/main/java/com/EventHive/repository/ShowRepository.java
@@ -0,0 +1,17 @@
+package com.EventHive.repository;
+
+
+import com.EventHive.entity.Show;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface ShowRepository extends JpaRepository<Show, Long> {
+
+}
